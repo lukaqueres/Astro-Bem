bfd192c9a86e64bfb4b5262a0303221e955c7d81
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-09-30 17:10:29 +0200

    Create articles.blade.php

---
+++ web/resources/views/articles.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Astro Bem | Articles</title>
+	    <link rel="stylesheet" type="text/css" href="/assets/css/app.css" />
+	    <script type="text/javascript" src="/assets/js/app.js"></script>
+	    <meta name="description" content="Site about 'Astro Bem' - group participating in 'CanSat' project" />
+	    <meta name="keywords" content="Astro, Bem, CanSat" />
+	    <meta name="author" content="Lukas" />
+	    <meta charset="UTF-8" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="container">
+            <div id="header" class="flex">
+            	<h2>Astro Bem</h2>
+               	<h3 class="flex-right">CanSat project</h3>
+            </div>
+        </div>
+    </body>
+</html>
