e841fb136938b95f72d4209d315b38ccdece1177
lukaqueres <lkocieln@gmail.com>
2022-10-01 22:52:43 +0200

    Update login.js

--- web/public/assets/js/login.js
+++ web/public/assets/js/login.js
@@ -7,7 +7,7 @@ function addAlert(text) {
     const alertDiv = document.createElement("div");
     const content = document.createTextNode(text);
     alertDiv.appendChild(content);
-    form.appendChild()
+    form.appendChild(alertDiv)
     return false;
 }
 
