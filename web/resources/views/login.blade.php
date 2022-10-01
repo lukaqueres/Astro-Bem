d2821bebe82cbda00dd4fe06d1437a0a88b93f69
lukaqueres <lkocieln@gmail.com>
2022-10-01 16:05:49 +0200

    Polish login page css

--- web/resources/views/login.blade.php
+++ web/resources/views/login.blade.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Astro Bem | Login</title>
-	    <link rel="stylesheet" type="text/css" href="/assets/css/app.css" />
-	    <script type="text/javascript" src="/assets/js/app.js"></script>
-	    <meta name="description" content="Site about 'Astro Bem' - group participating in 'CanSat' project" />
-	    <meta name="keywords" content="Astro, Bem, CanSat" />
-	    <meta name="author" content="Lukas" />
-	    <meta charset="UTF-8" />
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body id="login-page">
-        <div class="container">
-            <div id="login-container" class="xy-center">
-                <p class="head">Login</p>
-                <form id="login-form">
-                    <label><p>Login:</p><input type="text" placeholder="Input your email here"></input></label>
-                    <label><p>Password:</p><input type="password" placeholder="A safe place to input your password"></input></label>
-                </form>
-            </div>
-        </div>
-    </body>
+	<head>
+		<title>Astro Bem | Login</title>
+		<link rel="stylesheet" type="text/css" href="/assets/css/login.css" />
+		<script type="text/javascript" src="/assets/js/app.js"></script>
+		<meta name="description" content="Site about 'Astro Bem' - group participating in 'CanSat' project" />
+		<meta name="keywords" content="Astro, Bem, CanSat" />
+		<meta name="author" content="Lukas" />
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+	    <div id="container" class="xy-center">
+			<p class="head">Login</p>
+			<form>
+				<label><p>Login:</p><input type="text" placeholder="Input your email here"></input></label>
+				<label><p>Password:</p><input type="password" placeholder="A safe place to input your password"></input></label>
+                <button type="submit">Authorize</button>
+			</form>
+		</div>
+	</body>
 </html>
