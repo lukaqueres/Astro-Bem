7dfdf9592a28731ee54b3e5f27640bbc4f9192a4
lukaqueres <lkocieln@gmail.com>
2022-10-01 17:39:38 +0200

    Update login.blade.php

--- web/resources/views/login.blade.php
+++ web/resources/views/login.blade.php
@@ -11,7 +11,6 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
-    <script src="/assets/js/login.js"></script>
 	<body>
 		<div id="container" class="absolute-xy-center">
 			<p class="head">Login</p>
