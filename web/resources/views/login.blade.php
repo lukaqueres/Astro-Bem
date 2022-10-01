022752b0b699d34ae6fbc5a27cf00a89c21a3da2
lukaqueres <lkocieln@gmail.com>
2022-10-01 13:15:10 +0200

    Add background image on `login`

--- web/resources/views/login.blade.php
+++ web/resources/views/login.blade.php
@@ -10,7 +10,7 @@
 	    <meta charset="UTF-8" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
-    <body>
+    <body id="login-page">
         <div class="container">
             <div id="login-container" class="xy-center">
                 <p class="head">Login</p>
