4638e2b6ae645c11839acb7aa16d98e813ea7af3
lukaqueres <lkocieln@gmail.com>
2022-10-01 17:37:46 +0200

    Update login.blade.php

--- web/resources/views/login.blade.php
+++ web/resources/views/login.blade.php
@@ -11,11 +11,12 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
+    <script src="/assets/js/login.js"></script>
 	<body>
 		<div id="container" class="absolute-xy-center">
 			<p class="head">Login</p>
-			<form action="/dashboard/authenticate" onsubmit="return validate()">
-				<label for="email" >Login:</label><input type="email" id="email" name="email" placeholder="Input your email here" class="y-center" required></input>
+			<form action="/dashboard/authenticate">
+				<label for="email" >Login:</label><input type="email" id="email" name="email" placeholder="Insert your email here" class="y-center" required></input>
 				<label for="password" >Password:</label><input type="password" id="password" name="password" placeholder="A safe place to input your password" class="y-center" required></input>
 				<button type="submit" class="y-center">Authorize</button>
 			</form>
