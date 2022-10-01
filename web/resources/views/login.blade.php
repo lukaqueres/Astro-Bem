b824f5af8a697ae94192ee561288547ee4099cfc
lukaqueres <lkocieln@gmail.com>
2022-10-01 22:46:37 +0200

    Update login.blade.php

--- web/resources/views/login.blade.php
+++ web/resources/views/login.blade.php
@@ -11,13 +11,14 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
+    <script src="/assets/js/login.js"></script>
 	<body>
 		<div id="container" class="absolute-xy-center">
 			<p class="head">Login</p>
-			<form method="POST" action="{{ route('authenticate') }}">
+			<form method="POST" action="{{ route('authenticate') }}" onsubmit="return loginValidate()">
 				@csrf
-				<label for="email" >Email:</label><input type="email" id="email" name="email" placeholder="Insert your email here" class="y-center" required></input>
-				<label for="password" >Password:</label><input type="password" id="password" name="password" placeholder="A safe place to input your password" class="y-center" required></input>
+				<label for="email" >Email:</label><input type="email" id="email" name="email" placeholder="Insert your email here" class="y-center"></input>
+				<label for="password" >Password:</label><input type="password" id="password" name="password" placeholder="A safe place to input your password" class="y-center"></input>
 				<button type="submit" class="y-center">Authorize</button>
 			</form>
 			@if (session('status'))
