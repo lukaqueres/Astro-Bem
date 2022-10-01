08093d6711ebe4f0828a3845da09b44dcaba3bca
lukaqueres <lkocieln@gmail.com>
2022-10-01 16:13:18 +0200

    Update login.blade.php

--- web/resources/views/login.blade.php
+++ web/resources/views/login.blade.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>Astro Bem | Login</title>
 		<link rel="stylesheet" type="text/css" href="/assets/css/login.css" />
+		<link rel="stylesheet" type="text/css" href="/assets/css/app.css" />
 		<script type="text/javascript" src="/assets/js/app.js"></script>
 		<meta name="description" content="Site about 'Astro Bem' - group participating in 'CanSat' project" />
 		<meta name="keywords" content="Astro, Bem, CanSat" />
@@ -11,12 +12,12 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
-	    <div id="container" class="xy-center">
+		<div id="container" class="xy-center">
 			<p class="head">Login</p>
 			<form>
 				<label><p>Login:</p><input type="text" placeholder="Input your email here"></input></label>
 				<label><p>Password:</p><input type="password" placeholder="A safe place to input your password"></input></label>
-                <button type="submit">Authorize</button>
+				<button type="submit">Authorize</button>
 			</form>
 		</div>
 	</body>
