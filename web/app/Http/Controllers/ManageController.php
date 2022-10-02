ca63abba82b1025ad50746ae6c2c33828e1bac85
lukaqueres <lkocieln@gmail.com>
2022-10-02 19:05:46 +0200

    Update ManageController.php

--- web/app/Http/Controllers/ManageController.php
+++ web/app/Http/Controllers/ManageController.php
@@ -7,7 +7,7 @@ use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class ManageController extends Controller {
 
