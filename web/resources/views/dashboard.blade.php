2c97c66d14be099cdebbe1500ccd2543c8a2da59
lukaqueres <lkocieln@gmail.com>
2022-10-03 20:50:10 +0200

    Update dashboard.blade.php

--- web/resources/views/dashboard.blade.php
+++ web/resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
-    <script src="/assets/js/dasboard.js"></script>
+    <script src="/assets/js/dashboard.js"></script>
 	<body>
 		<div class="container">
 			<div id="aside-menu">
