6812cbd19952f5e82f876acc25c6ac4b0f78078e
lukaqueres <lkocieln@gmail.com>
2022-10-03 22:02:14 +0200

    Update UserManageController.php

--- web/app/Http/Controllers/UserManageController.php
+++ web/app/Http/Controllers/UserManageController.php
@@ -14,22 +14,19 @@ class UserManageController extends Controller {
     public function add(Request $request)
     {
         if ($request->missing('name') or $request->input('name') == '' ) {
-            return back()->with('status', 'Name is required, please input user\'s name.');
+            return back()->with('newUserStatus', 'Name is required, please input user\'s name.');
         }
         if ($request->missing('email') or $request->input('email') == '' ) {
-            return back()->with('status', 'Email not provided, please input users\'s email.');
+            return back()->with('newUserStatus', 'Email not provided, please input users\'s email.');
         }
         if ($request->missing('password') or $request->input('password') == '' ) {
-            return back()->with('status', 'Password not provided, please input users\'s password.');
+            return back()->with('newUserStatus', 'Password not provided, please input users\'s password.');
         }
-        $data['name'] = 'Test';
-        $data['email'] = 'test@tes';
-        $data['password'] = '123';
         
         User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']), 
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password), 
         ]);
         
         //DB::table('users')->insert(['name' => ‘admin’, 'email' => 'admin@email.com', 'password' => Hash::make('Admin@123'),]);
