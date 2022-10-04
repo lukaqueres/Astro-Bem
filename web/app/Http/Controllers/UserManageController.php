9e95e222f5c9fbe9da6412e4fa18b65c30a40c7e
lukaqueres <lkocieln@gmail.com>
2022-10-04 16:42:23 +0200

    Update UserManageController.php

--- web/app/Http/Controllers/UserManageController.php
+++ web/app/Http/Controllers/UserManageController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserManageController extends Controller {
 
