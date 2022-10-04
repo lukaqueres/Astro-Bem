4a004d3ae335c9c82643ca263fb4744e6d877c37
lukaqueres <lkocieln@gmail.com>
2022-10-04 17:49:34 +0200

    Update UserManageController.php

--- web/app/Http/Controllers/UserManageController.php
+++ web/app/Http/Controllers/UserManageController.php
@@ -15,20 +15,20 @@ class UserManageController extends Controller {
     public function add(Request $request)
     {
         if ($request->missing('name') or $request->input('name') == '' ) {
-            return back()->with('newUserStatus', 'Name is required, please input user\'s name.')->onlyInput($request->except('password'));
+            return back()->with('newUserStatus', 'Name is required, please input user\'s name.')->withInput($request->except('password'));
         }
         if ($request->missing('email') or $request->input('email') == '' ) {
-            return back()->with('newUserStatus', 'Email not provided, please input users\'s email.')->onlyInput($request->except('password'));
+            return back()->with('newUserStatus', 'Email not provided, please input users\'s email.')->withInput($request->except('password'));
         }
         if ($request->missing('password') or $request->input('password') == '' ) {
-            return back()->with('newUserStatus', 'Password not provided, please input users\'s password.')->onlyInput($request->except('password'));
+            return back()->with('newUserStatus', 'Password not provided, please input users\'s password.')->withInput($request->except('password'));
         }
         $users = User::all();
         foreach ( $users as $u ) { // - Check if id is always unique -
             if ( $request->input('email') == $u->email ) {
                 return back()->withErrors([
                     'newUserStatus' => 'Given email is already associated with another user.',
-                ])->onlyInput($request->except('password'));
+                ])->withInput($request->except('password'));
                 //return back()->with('newUserStatus', 'Given email is already associated with another user.')->onlyInput('name');
             }
 		}
@@ -43,6 +43,6 @@ class UserManageController extends Controller {
         
         //DB::table('users')->insert(['name' => ‘admin’, 'email' => 'admin@email.com', 'password' => Hash::make('Admin@123'),]);
         //DB::table('users')->insert(['name'=>'Test','email'=>'test@tes','password'=>Hash::make('123')]);
-        return back()->with('newUserStatus', 'User created.')->onlyInput($request->except('password'));
+        return back()->with('newUserStatus', 'User created.')->withInput($request->except('password'));
     }
 }
