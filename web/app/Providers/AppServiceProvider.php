a2d6db5b4340d821068ba59362d4ac5f5eabb3a2
lukaqueres <lkocieln@gmail.com>
2022-10-04 20:22:19 +0200

    Update AppServiceProvider.php

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -23,9 +23,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        if(env('FORCE_HTTPS',false)) { 
-            url()->forceScheme('https');
+        if ($this->isSecure()) {
+            \URL::forceSchema('https');
         }
+    }
+
+    public function isSecure()
+    {
+        $isSecure = false;
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+            $isSecure = true;
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+            $isSecure = true;
+        }
+
+        return $isSecure;
     }
 }
