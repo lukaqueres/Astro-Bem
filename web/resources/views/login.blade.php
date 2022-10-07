50093ab606524f5f0359ffe74a4f61bbd1ae3cd9
lukaqueres <lkocieln@gmail.com>
2022-10-07 21:15:48 +0200

    Update login.blade.php

--- web/resources/views/login.blade.php
+++ web/resources/views/login.blade.php
@@ -10,6 +10,7 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
+    <script src="/assets/js/form.js"></script>
     <script src="/assets/js/login.js"></script>
     <script type="text/javascript" src="/assets/js/app.js"></script>
 	<body>
