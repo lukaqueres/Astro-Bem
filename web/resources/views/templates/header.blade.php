f68bb34616adc6d84512c492de8842fa5de7717b
lukaqueres <lkocieln@gmail.com>
2022-10-09 21:57:32 +0200

    Delete header.blade.php

--- web/resources/views/templates/header.blade.php
+++
@@ -1,8 +0,0 @@
-@push('head')
-    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/app.css') }}" />
-    <meta name="author" content="Lukas" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="/assets/js/dashboard.js"></script>
-@endpush
