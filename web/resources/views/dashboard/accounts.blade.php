35acc5568eac5d2f77972aff951e22c78a599372
lukaqueres <lkocieln@gmail.com>
2022-10-10 12:07:50 +0200

    Update accounts.blade.php

--- web/resources/views/dashboard/accounts.blade.php
+++ web/resources/views/dashboard/accounts.blade.php
@@ -20,30 +20,6 @@
 					@empty
 						<div>There are no users</div>
 					@endforelse
-					@if (session('addUserStatus'))
-						<div class="new-user-status">
-							<p> {{ session('addUserStatus') }} </p>
-						</div>
-					@endif
-					@if (session('addUserError'))
-						<div id="add-new-user-reveal-button" class="hidden"><button onclick="showNewUserMenu()"><ion-icon name="add-outline"></ion-icon></button><p>Add user</p></div>
-						<div id="add-new-user-form" >
-					@else
-						<div id="add-new-user-reveal"><button onclick="showNewUserMenu()" class="x-center add-new-user-reveal-button"><ion-icon name="add-outline"></ion-icon></button><p>Add user</p></div>
-						<div id="add-new-user-form" class="hidden" >
-					@endif
-					<form id="addUser" method="POST" action="{{ route('addUser') }}" novalidate onsubmit="return validateNewUserForm(event)">
-						@csrf
-						<label for="name">Name:</label><input type="text" id="name" name="name" value="{{ old('name') }}"/>
-						<label for="email">Email:</label><input type="email" id="email" name="email" value="{{ old('email') }}"/>
-						<label for="password">Password:</label><input type="password" id="password" name="password"/>
-						<button type="submit">Add user</button>
-					</form>
-					@if (session('addUserError'))
-						<div class="new-user-error">
-						   <p> {{ session('addUserError') }} </p>
-						</div>
-					@endif
 					</div>
                 </div>
 			</div>
