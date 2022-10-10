732ee24fa1eb4c9cbf15fe4a1703a85ed1d8aad7
lukaqueres <lkocieln@gmail.com>
2022-10-10 11:49:00 +0200

    Create DashboardController.php

---
+++ web/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller {
+
+}
