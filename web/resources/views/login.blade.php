a1a9002d20f782de19905552c1155e1d11076c00
lukaqueres <lkocieln@gmail.com>
2022-10-10 11:23:36 +0200

    Update login.blade.php

--- web/resources/views/login.blade.php
+++ web/resources/views/login.blade.php
@@ -2,37 +2,42 @@
 <html>
 	<head>
 		<title>Astro Bem | Login</title>
-        <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/login.css') }}" />
-        @include('templates.head')
+		<link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/login.css') }}" />
+		@include('templates.head')
 	</head>
-    <script src="/assets/js/form.js"></script>
-    <script type="text/javascript" src="/assets/js/app.js"></script>
+	<script src="/assets/js/form.js"></script>
+	<script type="text/javascript" src="/assets/js/app.js"></script>
 	<body>
 		<div id="container" class="absolute-xy-center container">
 			<p class="head">Login</p>
-			<form method="POST" action="{{ route('authenticate') }}" onsubmit="return loginValidate(event)" novalidate>
+			<form method="POST" action="{{ route('authenticate') }}" onsubmit="return function () {
+				const form = new Form(e.target, 'error');
+					return form.validate({
+						'email': ['trim', 'required', 'email'],
+						'password': ['required'],
+				});}" novalidate> 
 				@csrf
 				<label for="email" >Email:</label><input type="email" id="email" name="email" placeholder="Insert your email here" title="Email input" class="x-center" value="{{ old('email') }}"></input>
 				<label for="password" >Password:</label><input type="password" id="password" name="password" placeholder="A safe place to input your password" title="Password input" class="x-center"></input>
 				<button type="submit" class="x-center" title="Send login credentials">Authorize</button>
 			</form>
-            <script>
-                function loginValidate(e) { // {{-- - This function validates login form. Must return `true` to send request - --}}
-                    const form = new Form(e.target, 'error');
-                    return form.validate({
-                        'email': ['trim', 'required', 'email'],
-                        'password': ['required'],
-                    });
-                }
-            </script>
-            <div id="error">
-			    @error('email')
-                    <div class="alert">{{ $message }}</div>
-                @enderror
-                @error('password')
-                    <div class="alert">{{ $message }}</div>
-                @enderror
-            </div>
+			<script>
+				function loginValidate(e) { // {{-- - This function validates login form. Must return `true` to send request `onsubmit="return loginValidate(event)"` - --}}
+					const form = new Form(e.target, 'error');
+					return form.validate({
+						'email': ['trim', 'required', 'email'],
+						'password': ['required'],
+					});
+				}
+			</script>
+			<div id="error">
+				@error('email')
+					<div class="alert">{{ $message }}</div>
+				@enderror
+				@error('password')
+					<div class="alert">{{ $message }}</div>
+				@enderror
+			</div>
 		</div>
 	</body>
 </html>
