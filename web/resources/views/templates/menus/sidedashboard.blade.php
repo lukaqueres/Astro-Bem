d77fd87473b1c0e8dc70ca8bbdbf95b6ef7657fa
lukaqueres <lkocieln@gmail.com>
2022-10-10 11:58:40 +0200

    Update sidedashboard.blade.php

--- web/resources/views/templates/menus/sidedashboard.blade.php
+++ web/resources/views/templates/menus/sidedashboard.blade.php
@@ -1,3 +1,4 @@
 <div class="side-menu">
-    <a href="{{ route('dashboard') }}/accounts">Accounts</a>
+    <a href="{{ route('dashboard') }}">Dashboard</a>
+    <a href="{{ route('accounts') }}">Accounts</a>
 </div>
