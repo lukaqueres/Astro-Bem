4199751a0db50ccd441bf58f8a19ee919ea40aae
lukaqueres <lkocieln@gmail.com>
2022-10-10 11:52:24 +0200

    Update DashboardController.php

--- web/app/Http/Controllers/DashboardController.php
+++ web/app/Http/Controllers/DashboardController.php
@@ -11,5 +11,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller {
-
+    public function accounts(Request $request)
+    {
+        return view('dashboard.accounts');
+    }
 }
