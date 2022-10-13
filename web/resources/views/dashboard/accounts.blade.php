ef3d9339326e6c35da402e53d03c11b5245c498f
lukaqueres <lkocieln@gmail.com>
2022-10-13 22:04:15 +0200

    Update accounts.blade.php

--- web/resources/views/dashboard/accounts.blade.php
+++ web/resources/views/dashboard/accounts.blade.php
@@ -12,21 +12,23 @@
 				<div class="cards-container">
 					@forelse ( App\Models\User::all() as $u )
 						<div class="card">
-                            @if ( $u->avatar )
-                                <img class="avatar" src="{{ $u->avatar }}" alt="User's profile picture'">
-                            @else 
-                                <img class="avatar" src="/assets/images/default_picture.png" alt="User's profile picture'">
-                            @endif
-                            <p class="title">{{ $u->name }}</p>
-							<p class="adnotation">Hover for details</p>
-                        </div>
-						<div class="card-content">
-							<p> Description </p>
-							<p>
-								<?php echo str_replace('-','.',substr($u->created_at, 0, -9)); ?>
-							</p>
-							<p class="email">{{ $u->email }}</p>
-							<a href="#"> See More </a>
+							<div class="main-content">
+								@if ( $u->avatar )
+									<img class="avatar" src="{{ $u->avatar }}" alt="User's profile picture'">
+								@else 
+									<img class="avatar" src="/assets/images/default_picture.png" alt="User's profile picture'">
+								@endif
+								<p class="title">{{ $u->name }}</p>
+								<p class="adnotation">Hover for details</p>
+							</div>
+							<div class="card-content">
+								<p> Description </p>
+								<p>
+									<?php echo str_replace('-','.',substr($u->created_at, 0, -9)); ?>
+								</p>
+								<p class="email">{{ $u->email }}</p>
+								<a href="#"> See More </a>
+							</div>
 						</div>
 					@empty
 						<div class="card"><p class="title">There are no users</p></div>
