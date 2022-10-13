cb135ae33aef81c365bc8de40c2bcc989842d0ae
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-10-13 14:03:35 +0200

    Update accounts.blade.php

--- web/resources/views/dashboard/accounts.blade.php
+++ web/resources/views/dashboard/accounts.blade.php
@@ -9,16 +9,19 @@
 		<div class="container">
 			@include('templates.menus.sidedashboard')
 			<div class="main">
-				<div id="users-details">
-				    <div class="columns-names">
-					    <p>Name</p>
-						<p>Email</p>
-					    <p class="create-date-column">Created</p>
-					</div>
+				<div id="cards-container">
 					@forelse ( App\Models\User::all() as $u )
-						<div class="user-details"><p class="name">{{ $u->name }}</p><p class="email">{{ $u->email }}</p><p class="create-date"><?php echo str_replace('-','.',substr($u->created_at, 0, -9)); ?></p></div>
+						<div class="card"><p class="title">{{ $u->name }}</p>
+                            <p class="email">{{ $u->email }}</p>
+                            <p class="preview"><?php echo str_replace('-','.',substr($u->created_at, 0, -9)); ?></p>
+                            <div class="content">
+                                <p> Description </p>
+                                <p><?php echo str_replace('-','.',substr($u->created_at, 0, -9)); ?></p>
+                                <a href="#"> See More </a>
+                            </div>
+                        </div>
 					@empty
-						<div>There are no users</div>
+						<div class="card"><p class="title">There are no users</p></div>
 					@endforelse
 					</div>
                 </div>
