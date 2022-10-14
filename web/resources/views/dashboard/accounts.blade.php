259496269817d2940f19f019d7c97bb039037cef
lukaqueres <lkocieln@gmail.com>
2022-10-14 16:20:49 +0200

    Update accounts.blade.php

--- web/resources/views/dashboard/accounts.blade.php
+++ web/resources/views/dashboard/accounts.blade.php
@@ -10,25 +10,21 @@
 			@include('templates.menus.sidedashboard')
 			<div class="main">
 				<div class="cards-container">
-					@forelse ( App\Models\User::all() as $u ) {{-- However it shows error here, it works just fine, DO NOT TOUCH IT  --}}
+					@forelse ( App\Models\User::all() as $u ) {{-- However it shows error here, it works just fine, DO - NOT - TOUCH - IT  --}}
 						<div class="card">
-							<div class="main-content">
-								@if ( $u->avatar )
-									<img class="avatar" src="{{ $u->avatar }}" alt="User's profile picture'">
-								@else 
-									<img class="avatar" src="/assets/images/default_picture.png" alt="User's profile picture'">
-								@endif
-								<p class="title">{{ $u->name }}</p>
-								<p class="adnotation">Hover for details</p>
-							</div>
-							<div class="card-content">
-								<p> Description </p>
-								<p>
-									<?php echo str_replace('-','.',substr($u->created_at, 0, -9)); ?>
-								</p>
-								<p class="email">{{ $u->email }}</p>
-								<a href="#" class="button-light"> See More </a>
-							</div>
+							@if ( $u->avatar )
+								<img class="avatar" src="{{ $u->avatar }}" alt="User's profile picture'">
+							@else 
+								<img class="avatar" src="/assets/images/default_picture.png" alt="User's profile picture'">
+							@endif
+							<p class="title">{{ $u->name }}</p>
+							<p class="adnotation">Hover for details</p>
+							<p> Description </p>
+							<p>
+							        @php echo str_replace('-','.',substr($u->created_at, 0, -9)); @endphp
+							</p>
+							<p class="email">{{ $u->email }}</p>
+							<a href="#" class="button-light"> See More </a>
 						</div>
 					@empty
 						<div class="card"><p class="title">There are no users</p></div>
