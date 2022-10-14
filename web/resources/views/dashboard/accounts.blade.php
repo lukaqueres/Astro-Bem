163205318b8051273a8fe2ef7fc3c7e8e48e0e02
lukaqueres <lkocieln@gmail.com>
2022-10-14 16:47:37 +0200

    Update accounts.blade.php

--- web/resources/views/dashboard/accounts.blade.php
+++ web/resources/views/dashboard/accounts.blade.php
@@ -12,27 +12,33 @@
 				<div class="cards-container">
 					@forelse ( App\Models\User::all() as $u ) {{-- However it shows error here, it works just fine, DO - NOT - TOUCH - IT  --}}
 						<div class="card">
-							@if ( $u->avatar )
-								<img class="avatar" src="{{ $u->avatar }}" alt="User's profile picture'">
-							@else 
-								<img class="avatar" src="/assets/images/default_picture.png" alt="User's profile picture'">
-							@endif
-							<p class="title">{{ $u->name }}</p>
-							<p class="adnotation">Hover for details</p>
-							<p> Description </p>
-							<p>
-							        @php echo str_replace('-','.',substr($u->created_at, 0, -9)); @endphp
-							</p>
-							<p class="email">{{ $u->email }}</p>
-							<a href="#" class="button-light"> See More </a>
+                            <div class="front">
+                                @if ( $u->avatar )
+                                <img class="avatar" src="{{ $u->avatar }}" alt="User's profile picture'" />
+                                @else
+                                <img class="avatar" src="/assets/images/default_picture.png" alt="User's profile picture'" />
+                                @endif
+                                <p class="title">{{ $u->name }}</p>
+                                <p class="adnotation">Hover for details</p>
+                            </div>
+                            <div class="details">
+                                <p> Description </p>
+                                <p>
+                                    @php echo str_replace('-','.',substr($u->created_at, 0, -9)); @endphp
+                                </p>
+                                <p class="email">{{ $u->email }}</p>
+                                <a href="#" class="button-light"> See More </a>
+                            </div>
 						</div>
 					@empty
 						<div class="card"><p class="title">There are no users</p></div>
 					@endforelse
                     <div class="card">
-						<img class="avatar" src="/assets/images/default_picture.png" alt="User's profile picture'" />
-						<p class="title"> Create user</p>
-						<a href="#" class="button-full">Add new user</a>
+                        <div class="front">
+                            <img class="avatar" src="/assets/images/default_picture.png" alt="User's profile picture'" />
+                            <p class="title"> Create user</p>
+                            <a href="#" class="button-full">Add new user</a>
+                        </div>
                     </div>
                 </div>
 			</div>
