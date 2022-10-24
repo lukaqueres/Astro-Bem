d3c6c2b5c7a0a91821535cf85b37f00097a09d94
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-10-24 16:08:55 +0200

    Move side menu file

---
+++ web/resources/views/dashboard/sidemenu.blade.php
@@ -0,0 +1,6 @@
+<div class="side-menu">
+    <ul style="list-style-type:none">
+        <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+        <li><a href="{{ route('accounts') }}">Accounts</a></li>
+    </ul>
+</div>
