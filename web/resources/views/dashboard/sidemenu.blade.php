f9b86df491b273a02a1ce2e751fe01e0abb9f8f8
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-10-24 16:23:56 +0200

    Change layout of aside menu

--- web/resources/views/dashboard/sidemenu.blade.php
+++ web/resources/views/dashboard/sidemenu.blade.php
@@ -1,6 +1,5 @@
 <div class="side-menu">
-    <ul style="list-style-type:none">
-        <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
-        <li><a href="{{ route('accounts') }}">Accounts</a></li>
-    </ul>
+    <p>{{ $user->name }}</p>
+    <a href="{{ route('dashboard') }}">Dashboard</a>
+    <a href="{{ route('accounts') }}">Accounts</a>
 </div>
