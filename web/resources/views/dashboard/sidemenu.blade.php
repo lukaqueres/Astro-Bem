44a8279b995d909a5d5dffb1d0e3daaaf8b36086
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-10-25 13:04:56 +0200

    Update sidemenu.blade.php

--- web/resources/views/dashboard/sidemenu.blade.php
+++ web/resources/views/dashboard/sidemenu.blade.php
@@ -5,6 +5,10 @@
             <p class="aside-name">{{ $user->name }}</p>
         </div>
         <p class="aside-tab">Dashboard</p>
+        <div class="user-links">
+            <button>Details</button>
+            <a href="{{ route('logout') }}">Logout</a>
+        </div>
     </div>
     <a class="aside-link" href="{{ route('dashboard') }}">Dashboard</a>
     <a class="aside-link" href="{{ route('accounts') }}">Accounts</a>
