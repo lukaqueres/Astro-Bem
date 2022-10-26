9b0213e9617cded1cc3bf22bea611372c42fe1cf
Golombs <xenti12pl@gmail.com>
2022-10-26 14:48:18 +0200

    fix layout

--- web/resources/views/dashboard/accounts.blade.php
+++ web/resources/views/dashboard/accounts.blade.php
@@ -11,62 +11,37 @@
 		<div class="container">
 			@include('dashboard.sidemenu')
 			<div class="main">
-				<div class="cards-container">
+				{{-- <div class="cards-container"> --}}
+                <div class="grid">
 					@forelse ( App\Models\User::all() as $u ) {{-- However it shows error here, it works just fine, DO - NOT - TOUCH - IT  --}}
-					
-					<div class="grid">
-
-            <div class="konto">
-
-            <p>siema</p>
-            </div>
-
-            
-            <div class="konto">
-                <img src="jo.png" alt="">
-                
-                <div class="nazwa">
-                    
-                    <h2>nazwa</h2>
-                    <p> nazwa użytkownika</p>
-
-
-                </div>
-                <div class="OS">
-
-                    <h2>Osiagnięcia</h2>
-                    <p>w życiu
-                    dużo osiągnąłem
-                    czyli nic</p>
-
-                </div>
-                <div class="EM">
-
-                    <h2>E-mail</h2>
-                    <p>email</p>
-                </div>
-
-            </div>
-
-
-
-
-         </div>
-        
-
-        </div>
-						
+                        <div class="konto">
+                            <p>siema</p>
+                        </div>
+                        <div class="konto">
+                            <img src="jo.png" alt="">
+                            <div class="nazwa">
+                                <h2>nazwa</h2>
+                                <p> nazwa użytkownika</p>
+                            </div>
+                            <div class="OS">
+                                <h2>Osiagnięcia</h2>
+                                <p>w życiu </br>dużo osiągnąłem </br>czyli nic</p>
+                            </div>
+                            <div class="EM">
+                                <h2>E-mail</h2>
+                                <p>email</p>
+                            </div>
+                        </div>	
 					@empty
 						<div class="card"><p class="title">There are no users</p></div>
 					@endforelse
-                    	<div class="card">
+                    <div class="card">
                         <div class="front">
                             <img class="avatar" src="/assets/images/default_picture.png" alt="User's profile picture'" />
                             <p class="title"> Create user</p>
                             <a href="{{ route('accounts_create') }}" class="button-full">Add new user</a>
                         </div>
-                    	</div>
-					
+                    </div>
                 </div>
 			</div>
 		</div>
