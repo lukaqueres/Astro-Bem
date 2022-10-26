505335d4abef0b29b5faa593ed8f24c0f6faadff
lukaqueres <lkocieln@gmail.com>
2022-10-26 23:18:20 +0200

    Update home.blade.php

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -2,17 +2,15 @@
 <html>
     <head>
         <title>Astro Bem | Home</title>
-	    <link rel="stylesheet" type="text/css" href="/assets/css/app.css" />
-	    <script type="text/javascript" src="/assets/js/app.js"></script>
 	    <meta name="description" content="Site about 'Astro Bem' - group participating in 'CanSat' project" />
 	    <meta name="keywords" content="Astro, Bem, CanSat" />
-	    <meta name="author" content="Lukas" />
-	    <meta charset="UTF-8" />
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @include('templates.head')
+        <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/home.css') }}" />
     </head>
     <body>
         <div class="container">
             <div id="header" class="flex">
+                <img src="/assets/images/logo.png"/>
             	<h2>Astro Bem</h2>
                	<a class="button flex-right">Articles</a>
                 <a class="button button-style-2">About Us</a>
