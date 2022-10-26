f90e275c640caef7b7b60d904793b63da30a00d7
lukaqueres <lkocieln@gmail.com>
2022-10-26 23:20:30 +0200

    Update home.blade.php

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <body>
         <div class="container">
             <div id="header" class="flex">
-                <img src="/assets/images/logo.png"/>
+                <img class="logo" src="/assets/images/logo.png"/>
             	<h2>Astro Bem</h2>
                	<a class="button flex-right">Articles</a>
                 <a class="button button-style-2">About Us</a>
