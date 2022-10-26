08f617c7c28b8a4872308545b0abf57bf7cb0fab
Golombs <xenti12pl@gmail.com>
2022-10-26 23:03:20 +0200

    dasda
    dasd

--- web/resources/views/dashboard/accounts.blade.php
+++ web/resources/views/dashboard/accounts.blade.php
@@ -35,7 +35,16 @@
                             </div>
                         </div>	
 
-					
+					@empty
+						<div class="card"><p class="title">There are no users</p></div>
+					@endforelse
+                    <div class="card">
+                        <div class="front">
+                            <img class="avatar" src="/assets/images/default_picture.png" alt="User's profile picture'" />
+                            <p class="title"> Create user</p>
+                            <a href="{{ route('accounts_create') }}" class="button-full">Add new user</a>
+                        </div>
+                    </div>
                 </div>
 			</div>
 		</div>
