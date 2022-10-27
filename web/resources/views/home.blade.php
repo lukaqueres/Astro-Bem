cd45957e7710e8ede502a207790365d65c80b73c
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-10-27 11:27:10 +0200

    Update home.blade.php

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -1,22 +1,34 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Astro Bem | Home</title>
-	    <link rel="stylesheet" type="text/css" href="/assets/css/app.css" />
-	    <script type="text/javascript" src="/assets/js/app.js"></script>
-	    <meta name="description" content="Site about 'Astro Bem' - group participating in 'CanSat' project" />
-	    <meta name="keywords" content="Astro, Bem, CanSat" />
-	    <meta name="author" content="Lukas" />
-	    <meta charset="UTF-8" />
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <div class="container">
-            <div id="header" class="flex">
-            	<h2>Astro Bem</h2>
-               	<a class="button flex-right">Articles</a>
-                <a class="button button-style-2">About Us</a>
-            </div>
+<head>
+    <title>Astro Bem | Home</title>
+    <meta name="description" content="Site about 'Astro Bem' - group participating in 'CanSat' project" />
+    <meta name="keywords" content="Astro, Bem, CanSat" />
+    @include('templates.head')
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/home.css') }}" />
+</head>
+<body>
+    <div class="container">
+        <div id="header" class="flex">
+            <img class="logo" src="/assets/images/logo.png" />
+            <h2>Astro Bem</h2>
+            {{--
+            <a class="button flex-right">Articles</a>
+            <a class="button button-style-2">About Us</a>
+            --}}
         </div>
-    </body>
+        <div class="image-holder">
+            <img class="image" src="assets/images/bckg_pic.png" />
+        </div>
+        <div class="back-ground-color">
+            <h1 class="header"> Welcome to Astrobem, school space science passionates </h1>
+            <p class="text">
+                We are working in our school, cooperationg with many teachers of various subjects.<br />
+                Currently we are participating in Polish edition of <i>CanSat</i> project, that is organizated by POLSA - Polish Space Agency.
+
+					Now we are right after sending our PDR documentation - first step in order to qualify to next stage of competition.
+            </p>
+        </div>
+    </div>
+</body>
 </html>
