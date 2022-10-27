149a16ac5e8de5bbc5eb5487c0302ed3ac00d7ff
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-10-27 10:28:47 +0200

    Update home.blade.php

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Astro Bem | Home</title>
-	    <meta name="description" content="Site about 'Astro Bem' - group participating in 'CanSat' project" />
-	    <meta name="keywords" content="Astro, Bem, CanSat" />
-        @include('templates.head')
-        <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/home.css') }}" />
-    </head>
-    <body>
-        <div class="container">
-            <div id="header" class="flex">
-                <img class="logo" src="/assets/images/logo.png"/>
-            	<h2>Astro Bem</h2>
-               	{{--
-                <a class="button flex-right">Articles</a>
-                <a class="button button-style-2">About Us</a>
-                --}}
-            </div>
-            <div class="image-holder">
-                <img src="assets/images/"/>
-            </div>
-        </div>
-    </body>
+	<head>
+		<title>Astro Bem | Home</title>
+		<meta name="description" content="Site about 'Astro Bem' - group participating in 'CanSat' project" />
+		<meta name="keywords" content="Astro, Bem, CanSat" />
+		@include('templates.head')
+		<link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/home.css') }}" />
+	</head>
+	<body>
+		<div class="container">
+			<div id="header" class="flex">
+				<img class="logo" src="/assets/images/logo.png"/>
+				<h2>Astro Bem</h2>
+				{{--
+				<a class="button flex-right">Articles</a>
+				<a class="button button-style-2">About Us</a>
+				--}}
+			</div>
+			<div class="image-holder">
+				<img src="assets/images/"/>
+			</div>
+		</div>
+	</body>
 </html>
