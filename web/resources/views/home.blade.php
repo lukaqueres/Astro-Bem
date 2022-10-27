51e806437c36ce746196a6165bb94bde15143ee0
Lucas <57031937+lukaqueres@users.noreply.github.com>
2022-10-27 11:13:19 +0200

    Add background

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -20,13 +20,15 @@
 			<div class="image-holder">
                 <img class="image" src="assets/images/bckg_pic.png" />
 			</div>
-			<h1 class="header"> Welcome to Astrobem, school space science passionates </h1>
-			<p class="text">
-				We are working in our school, cooperationg with many teachers of various subjects.</br>
-				Currently we are participating in Polish edition of <i>CanSat</i> project, that is organizated by POLSA - Polish Space Agency.
+			<div class="back-ground-color">
+				<h1 class="header"> Welcome to Astrobem, school space science passionates </h1>
+				<p class="text">
+					We are working in our school, cooperationg with many teachers of various subjects.</br>
+					Currently we are participating in Polish edition of <i>CanSat</i> project, that is organizated by POLSA - Polish Space Agency.
 
-				Now we are right after sending our PDR documentation - first step in order to qualify to next stage of competition.
-			</p>
+					Now we are right after sending our PDR documentation - first step in order to qualify to next stage of competition.
+				</p>
+			</div>
 		</div>
 	</body>
 </html>
