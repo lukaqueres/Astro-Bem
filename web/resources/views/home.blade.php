e9bb7f1adbe67fa48160ad828e919c7cc071be88
Golombs <xenti12pl@gmail.com>
2022-11-16 10:06:33 +0100

    11
    1

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -12,10 +12,10 @@
 			<div id="header" class="flex">
 				<img class="logo" src="/assets/images/logo.png"/>
 				<h2>Astro Bem</h2>
-				{{--
+				
 				<a class="button flex-right">Articles</a>
 				<a class="button button-style-2">About Us</a>
-				--}}
+				
 			</div>
 			<div class="image-holder">
                 <img class="image" src="assets/images/bckg_pic.png" />
