5f92e006925a67677237b3da9e19653c51328ad5
Golombs <xenti12pl@gmail.com>
2022-11-16 11:48:42 +0100

    16.11

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -19,6 +19,64 @@
 				<a class="button button-style-2">About Us</a>
 				
 			</div>
+
+			<!-- Przewijanie Zdjęc Hero Slider -->
+			<div class="slider">
+				<!-- Aby Dodać Zdjęcie trzeba dodać nowy element myslide fade oraz nową kropkę ! -->
+				<div class="myslide fade">
+					<div class="txt">
+						<h1>Jakiś Tytuł</h1>
+						<p>Lorem Impum Dolor Si Amen</p>
+					</div>
+					<img src="img1.jpg" style="width: 100%; height: 100%;">
+				</div>
+				
+				<div class="myslide fade">
+					<div class="txt">
+						<h1>Jakiś Tytuł</h1>
+						<p>Lorem Impum Dolor Si Amen</p>
+					</div>
+					<img src="img2.jpg" style="width: 100%; height: 100%;">
+				</div>
+				
+				<div class="myslide fade">
+					<div class="txt">
+						<h1>Jakiś Tytuł</h1>
+						<p>Lorem Impum Dolor Si Amen</p>
+					</div>
+					<img src="img3.jpg" style="width: 100%; height: 100%;">
+				</div>
+				
+				<div class="myslide fade">
+					<div class="txt">
+						<h1>Jakiś Tytuł</h1>
+						<p>Lorem Impum Dolor Si Amen</p>
+					</div>
+					<img src="img4.jpg" style="width: 100%; height: 100%;">
+				</div>
+				
+				<div class="myslide fade">
+					<div class="txt">
+						<h1>Jakiś Tytuł</h1>
+						<p>Lorem Impum Dolor Si Amen</p>
+					</div>
+					<img src="img5.jpg" style="width: 100%; height: 100%;">
+				</div>
+
+				<ion-icon name="caret-forward" class="next"  onclick="plusSlides(1)"></ion-icon>
+				<ion-icon name="caret-back" class="prev"  onclick="plusSlides(-1)"></ion-icon>
+				
+				<div class="dotsbox" style="text-align:center">
+					<span class="dot" onclick="currentSlide(1)"></span>
+					<span class="dot" onclick="currentSlide(2)"></span>
+					<span class="dot" onclick="currentSlide(3)"></span>
+					<span class="dot" onclick="currentSlide(4)"></span>
+					<span class="dot" onclick="currentSlide(5)"></span>
+				</div>
+			</div>
+			<script src="assets/js/HeroSliderJS.js"></script>
+			<!-- Przewijanie Zdjęc Hero Slider (Koniec) -->
+
 			<div class="image-holder">
                 <img class="image" src="assets/images/bckg_pic.png" />
 			</div>
