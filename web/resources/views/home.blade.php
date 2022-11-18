6d353ff1e90c0853e85474f26f368dd846547882
BlyatTukan <Overnex03@gmail.com>
2022-11-18 11:50:52 +0100

    18,11

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -79,51 +79,62 @@
 			<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 			<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 			<!-- Przewijanie Zdjęc Hero Slider (Koniec) -->
- 
-			
-			<div class="back-ground-color">
-				<h1 class="header"> Welcome to Astrobem, school space science passionates </h1>
-				<p class="text">
-					We are working in our school, cooperationg with many teachers of various subjects.</br>
-					Currently we are participating in Polish edition of <i>CanSat</i> project, that is organizated by POLSA - Polish Space Agency.
+			<div class="welcome">
+		<h1>Welcome to Astrobem, school space science passionates</h1>
+		<p>We are working in our school, cooperationg with many teachers of various subjects.
+			Currently we are participating in Polish edition of CanSat project, that is organizated by POLSA - Polish Space Agency. Now we are right after sending our PDR documentation - first step in order to qualify to next stage of competition.</p>
+	</div>
+	<h1 class="header">Aktualności</h1>
+	<div class="blog-list">
 
-					Now we are right after sending our PDR documentation - first step in order to qualify to next stage of competition.
-				</p>
+		<div class="blog-post">
+			<div class="blog-post_img">
+				<img src="https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU3fHx0ZWNobm9sb2d5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+					alt="">
 			</div>
-			<h1 class="header">Aktualności:</h1>
-			<div class="container">
-
-					<!--<div class="konkurs">
-				<h1 class="header">Konkurs ,, Astro-Bem ,, Zaćmienie Słońca widziane smartfonem”.</h1>
-					<p class="text">
-					Dnia 25-10-2022 obserwowaliśmy zaćmienie słońca.
-					Okazało się, że wielu z nas zrobiło śliczne zdjęcia, dlatego ogłaszamy konkurs na najładniejszą fotografię.
-					Zdjęcia proszę nadsyłać na dysk Astro-Bem.
-				</p class = "text">
-					<center>
- 					<a class= "linkk" href ="https://drive.google.com/drive/folders/1cv1l7dq6O7w3xKJSpBFUQGLz-OV0rApG"> LINK DO DYSKU </a>
-				</center>
-					<p class = "text">
-					Aby przesłać zdjęcie uczestnik musi skorzystać z poczty szkolnej.
-					 Termin nadsyłania zdjęć do końca listopada / 30.11.2022/. 
- 					Ogłoszenie wyników 15.12.2022.
-					 Na zwycięzców czekają nagrody.
-					
-					</p>
-				</div>-->
-
-				<div class="aktualnosci">
-					<div class="aktualnosci-title">
-						<h1>Konkurs ,, Astro-Bem ,, Zaćmienie Słońca widziane smartfonem”.</h1>
-					</div>
-					<p>Dnia 25-10-2022 obserwowaliśmy zaćmienie słońca. Okazało się, że wielu z nas zrobiło śliczne zdjęcia, dlatego ogłaszamy konkurs na najładniejszą fotografię. Zdjęcia proszę nadsyłać na dysk Astro-Bem.</p>
-					<br><a href ="https://drive.google.com/drive/folders/1cv1l7dq6O7w3xKJSpBFUQGLz-OV0rApG">LINK DO DYSKU</a><br><br>
-					<p>Aby przesłać zdjęcie uczestnik musi skorzystać z poczty szkolnej. Termin nadsyłania zdjęć do końca listopada / 30.11.2022/. Ogłoszenie wyników 15.12.2022. Na zwycięzców czekają nagrody. </p>
+			<div class="blog-post_info">
+				<div class="blog-post_date">
+					<span>Nov 12 2021</span>
 				</div>
-
+				<h1 class="blog-post_title">Konkurs ,, Astro-Bem ,, Zaćmienie Słońca widziane smartfonem”</h1>
+				<p class="blog-post_text">
+					Dnia 25-10-2022 obserwowaliśmy zaćmienie słońca. Okazało się, że wielu z nas zrobiło śliczne zdjęcia, dlatego ogłaszamy konkurs na najładniejszą fotografię. Zdjęcia proszę nadsyłać na dysk Astro-Bem.
+					<br><br>
+					Aby przesłać zdjęcie uczestnik musi skorzystać z poczty szkolnej. Termin nadsyłania zdjęć do końca listopada / 30.11.2022/. Ogłoszenie wyników 15.12.2022. Na zwycięzców czekają nagrody.
+				</p>
+				<a href="https://drive.google.com/drive/folders/1cv1l7dq6O7w3xKJSpBFUQGLz-OV0rApG" class="blog-post_cta">Link Do Dysku</a>
 			</div>
 		</div>
-			
+		<div class="blog-post">
+			<div class="blog-post_img">
+				<img src="https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU3fHx0ZWNobm9sb2d5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+					alt="">
+			</div>
+			<div class="blog-post_info">
+				<div class="blog-post_date">
+					<span>Nov 12 2021</span>
+				</div>
+				<h1 class="blog-post_title">Konkurs ,, Astro-Bem ,, Zaćmienie Słońca widziane smartfonem”</h1>
+				<p class="blog-post_text">
+					Dnia 25-10-2022 obserwowaliśmy zaćmienie słońca. Okazało się, że wielu z nas zrobiło śliczne zdjęcia, dlatego ogłaszamy konkurs na najładniejszą fotografię. Zdjęcia proszę nadsyłać na dysk Astro-Bem.
+					<br><br>
+					Aby przesłać zdjęcie uczestnik musi skorzystać z poczty szkolnej. Termin nadsyłania zdjęć do końca listopada / 30.11.2022/. Ogłoszenie wyników 15.12.2022. Na zwycięzców czekają nagrody.
+				</p>
+				<a href="https://drive.google.com/drive/folders/1cv1l7dq6O7w3xKJSpBFUQGLz-OV0rApG" class="blog-post_cta">Link Do Dysku</a>
+			</div>
+		</div>
+	</div>
+
+
+	<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+		<defs><path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path></defs>
+		<g class="parallax">
+			<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7"></use>
+			<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)"></use>
+			<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)"></use>
+			<use xlink:href="#gentle-wave" x="48" y="7" fill="#fff"></use>
+		</g>
+	</svg>
 
 		 
 	</body>
