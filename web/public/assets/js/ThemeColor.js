50b03a4afae924075d1685d01f18534e01ec267b
BlyatTukan <Overnex03@gmail.com>
2022-11-20 11:41:00 +0100

    Update ThemeColor.js

--- web/public/assets/js/ThemeColor.js
+++ web/public/assets/js/ThemeColor.js
@@ -5,18 +5,18 @@ function ChangeTheme()
 
     if(document.body.classList.contains("dark-theme"))
     {
-        r.style.setProperty("--base-color-level-1", "#eee");
-        r.style.setProperty("--base-color-level-2", "#ddd");
-        r.style.setProperty("--base-color-level-3", "#ccc");
-        r.style.setProperty("--text-color-level-1", "#222");
-        r.style.setProperty("--text-color-level-2", "#888");
-    }
-    else
-    {
         r.style.setProperty("--base-color-level-1", "#222");
         r.style.setProperty("--base-color-level-2", "#333");
         r.style.setProperty("--base-color-level-3", "#444");
         r.style.setProperty("--text-color-level-1", "#eee");
         r.style.setProperty("--text-color-level-2", "#999");
+    }
+    else
+    {
+        r.style.setProperty("--base-color-level-1", "#eee");
+        r.style.setProperty("--base-color-level-2", "#ddd");
+        r.style.setProperty("--base-color-level-3", "#ccc");
+        r.style.setProperty("--text-color-level-1", "#222");
+        r.style.setProperty("--text-color-level-2", "#888");
     }
 }
