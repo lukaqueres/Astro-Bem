d1b79a48022dc897ccc387056b26c375b2e7321d
BlyatTukan <Overnex03@gmail.com>
2022-11-23 23:04:32 +0100

    2311

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -47,7 +47,7 @@
 						<li class="nav__item"><a href="/" class="nav__link active">Home</a></li>
 						<li class="nav__item"><a href="https://esero.kopernik.org.pl/konkurs-cansat/" class="nav__link">CanSat</a></li>
 						<li class="nav__item"><a href= "/articles" class="nav__link">Articles</a></li>
-						<li class="nav__item"><a href= "/kosmicznaSzkola" class="nav__link">Kosmiczna szkoła</a></li>
+						<li class="nav__item"><a href= "/kosmiczna-szkola" class="nav__link">Kosmiczna szkoła</a></li>
 						<li class="nav__item"><a href= "/about-us" class="nav__link">About Us</a></li>
 					</ul>
 				</div>
