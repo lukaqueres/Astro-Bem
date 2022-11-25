bb7702e271d7ac798ff6a6fdadb8596a10cf637f
BlyatTukan <Overnex03@gmail.com>
2022-11-25 15:34:12 +0100

    2511

--- web/resources/views/articles.blade.php
+++ web/resources/views/articles.blade.php
@@ -61,37 +61,8 @@
 	</header>
 
 			
-    @include('templates.header')dist/ionicons/ionicons.js"></script>
-			<!-- Przewijanie Zdjęc Hero Slider (Koniec) -->
-
-	<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-		<defs><path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path></defs>
-		<g class="parallax">
-			<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7"></use>
-			<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)"></use>
-			<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)"></use>
-			<use xlink:href="#gentle-wave" x="48" y="7" fill="#fff"></use>
-		</g>
-	</svg>
-	<footer>
-		<ul class="social_icon">
-			<li><a href="#"><ion-icon name="logo-buffer"></ion-icon></a></li>
-			<li><a href="#"><ion-icon name="logo-buffer"></ion-icon></a></li>
-			<li><a href="#"><ion-icon name="logo-buffer"></ion-icon></a></li>
-			<li><a href="#"><ion-icon name="logo-buffer"></ion-icon></a></li>
-		</ul>
-		<ul class="menu">
-			<li><a href="#">Podstrona_1</a></li>
-			<li><a href="#">Podstrona_2</a></li>
-			<li><a href="#">Podstrona_3</a></li>
-		</ul>
-		<p>2022 Astro Bem | All Rights Reserved</p>
-	</footer>
-
-		 
-	<script src="assets/js/HeroSliderJS.js"></script>
-	<script src="assets/js/Navbar.js"></script>
-	<script src="assets/js/ThemeColor.js"></script>
+    @include('templates.header')
+    @include('templates.footer')
 	</body>
 
 </html>
--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -62,6 +62,7 @@
 
 			
     @include('templates.header')
+	
 			<div class="welcome">
 		<h1>Welcome to Astrobem, school space science passionates</h1>
 		<p>We are working in our school, cooperationg with many teachers of various subjects.
@@ -109,34 +110,8 @@
 	</div>
 
 
-	<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-		<defs><path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path></defs>
-		<g class="parallax">
-			<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7"></use>
-			<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)"></use>
-			<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)"></use>
-			<use xlink:href="#gentle-wave" x="48" y="7" fill="#fff"></use>
-		</g>
-	</svg>
-	<footer>
-		<ul class="social_icon">
-			<li><a href="#"><ion-icon name="logo-buffer"></ion-icon></a></li>
-			<li><a href="#"><ion-icon name="logo-buffer"></ion-icon></a></li>
-			<li><a href="#"><ion-icon name="logo-buffer"></ion-icon></a></li>
-			<li><a href="#"><ion-icon name="logo-buffer"></ion-icon></a></li>
-		</ul>
-		<ul class="menu">
-			<li><a href="#">Podstrona_1</a></li>
-			<li><a href="#">Podstrona_2</a></li>
-			<li><a href="#">Podstrona_3</a></li>
-		</ul>
-		<p>2022 Astro Bem | All Rights Reserved</p>
-	</footer>
 
-		 
-	<script src="assets/js/HeroSliderJS.js"></script>
-	<script src="assets/js/Navbar.js"></script>
-	<script src="assets/js/ThemeColor.js"></script>
+    @include('templates.footer')
 	</body>
 
 </html>
