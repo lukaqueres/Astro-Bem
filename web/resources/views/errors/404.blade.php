cb3b2f493dac467f1fa767e2f1e16c7cb0a06f28
BlyatTukan <Overnex03@gmail.com>
2022-11-27 16:35:56 +0100

    Update 404.blade.php

--- web/resources/views/errors/404.blade.php
+++ web/resources/views/errors/404.blade.php
@@ -14,7 +14,7 @@
 </style>
 
 <body>
-	<h1 id="H1" style="color: var(--gradient-text); text-align: center;">Error 404</h1>
+	<h1 id="H1" style="color: var(--gradient-text); text-align: center; font-size: 60px;">Error 404</h1>
 	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 	@include('templates.footer')
@@ -25,8 +25,8 @@
 
 		// H1
 		let h1 = document.querySelector('#H1');
-		h1.style.marginTop = (window.innerHeight - (height + 100) / 2) + "px";
-		h1.style.marginBottom = (window.innerHeight - (height + 100) / 2) + "px";
+		h1.style.marginTop = (window.innerHeight - (height + 200) / 2) + "px";
+		h1.style.marginBottom = (window.innerHeight - (height + 200) / 2) + "px";
 	</script>
 </body>
 
