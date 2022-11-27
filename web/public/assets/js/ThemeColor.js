4c101cf9a57a4b505b9fa465f892bd1e8929def9
BlyatTukan <Overnex03@gmail.com>
2022-11-27 15:09:36 +0100

    Update ThemeColor.js

--- web/public/assets/js/ThemeColor.js
+++ web/public/assets/js/ThemeColor.js
@@ -1,10 +1,31 @@
+// Dodanie Localstorage jeżeli nie ma
+if (localStorage.getItem("theme") === null) 
+{
+    localStorage.setItem("theme", "light");
+}
+
+// Ustaiwa Odpowiedni Tryb
+if (localStorage.getItem("theme") === "dark") 
+{
+    document.body.classList.add("dark-theme");
+    SetTheme();
+}
+
+// Zmiana Theme po kliknięciu
 function ChangeTheme()
 {
     var r = document.querySelector(":root");
     document.body.classList.toggle("dark-theme");
 
+    SetTheme();
+}
+
+// Zmiana Prawdziwa
+function SetTheme()
+{
     if(document.body.classList.contains("dark-theme"))
     {
+        localStorage.setItem("theme", "dark");
         r.style.setProperty("--base-color-level-1", "#222");
         r.style.setProperty("--base-color-level-2", "#333");
         r.style.setProperty("--base-color-level-3", "#444");
@@ -13,6 +34,7 @@ function ChangeTheme()
     }
     else
     {
+        localStorage.setItem("theme", "light");
         r.style.setProperty("--base-color-level-1", "#eee");
         r.style.setProperty("--base-color-level-2", "#ddd");
         r.style.setProperty("--base-color-level-3", "#ccc");
