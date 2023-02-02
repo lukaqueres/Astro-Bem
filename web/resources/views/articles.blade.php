5c8390a6c978507ac0389986096c50095d597530
Golombs <xenti12pl@gmail.com>
2023-02-03 00:09:47 +0100

    Update articles.blade.php

--- web/resources/views/articles.blade.php
+++ web/resources/views/articles.blade.php
@@ -64,7 +64,7 @@
 
 			<div class="blog-post">
 				<div class="blog-post_img">
-				<source src="02.02.2023" type="video/mp4">
+				<source src="02.02.2023.mp4" type="video/mp4">
 				</div>
 				<div class="blog-post_info">
 					<div class="blog-post_date">
