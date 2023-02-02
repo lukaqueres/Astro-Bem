0f9e40b2560f7546d65828b79e16a8ddeab5f023
Golombs <xenti12pl@gmail.com>
2023-02-02 15:27:07 +0100

    23

--- web/resources/views/aboutUs.blade.php
+++ web/resources/views/aboutUs.blade.php
@@ -74,7 +74,7 @@
                     <span class="overlay"></span>
 
                     <div class="card-image">
-                        <img src="assets/images/person/rafal-durka.png" alt="" class="card-img">
+                        <img src="assets/images/person/Krzyczkowska.jpg" alt="" class="card-img">
                     </div>
                 </div>
 
