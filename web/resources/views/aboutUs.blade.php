ab8381b5849d4c56fc00e470754053819eb14c7c
Golombs <xenti12pl@gmail.com>
2023-02-02 13:12:31 +0100

    2
    2

--- web/resources/views/aboutUs.blade.php
+++ web/resources/views/aboutUs.blade.php
@@ -89,7 +89,7 @@
                     <span class="overlay"></span>
 
                     <div class="card-image">
-                        <img src="assets/images/person/rafal-durka.png" alt="" class="card-img">
+                        <img src="assets/images/person/kocielnik.jpg" alt="" class="card-img">
                     </div>
                 </div>
 
