bfd5dfda1492f5df806f2c750e7aae77a3ab9924
BlyatTukan <Overnex03@gmail.com>
2023-02-06 16:08:15 +0100

    Update home.blade.php

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -78,10 +78,10 @@
 					</div>
 					<h1 class="blog-post_title">XXX</h1>
 					<p class="blog-post_text">
-						XXX
+						We would like to invite you to our Twitter account, where we will as well post about our project's progress!
 					</p>
 					<a href="XXX"
-						class="blog-post_cta">XXX</a>
+						class="blog-post_cta">Facebook Post Link</a>
 				</div>
 			</div>
 			<div class="blog-post">
@@ -95,10 +95,10 @@
 					</div>
 					<h1 class="blog-post_title">XXX</h1>
 					<p class="blog-post_text">
-						XXX
+						On 28th January our group decided to go for a series of field tests of radio communication in Parzniew (Masovian Voivodeship). The tests were successful despite demanding and unsettled weather and we managed to establish a connection between the broadcasting and receiving station on a 1 km distance.
 					</p>
 					<a href="XXX"
-						class="blog-post_cta">XXX</a>
+						class="blog-post_cta">Facebook Post Link</a>
 				</div>
 			</div>
 			<div class="blog-post">
@@ -112,10 +112,10 @@
 					</div>
 					<h1 class="blog-post_title">XXX</h1>
 					<p class="blog-post_text">
-						XXX
+						On 13th January 2023 our group met with our mentor Paweł Witkowski. In a meeting we had been discussing ways of radio communication between CanSat and our ground base. This gathering was a huge brainstorm, opening up for us many new possibilities of communication and building antennas for the project, but also many new secondary goals, which we can write about in our documentation.
 					</p>
 					<a href="XXX"
-						class="blog-post_cta">XXX</a>
+						class="blog-post_cta">Facebook Post Link</a>
 				</div>
 			</div>
 			<div class="blog-post">
@@ -129,10 +129,10 @@
 					</div>
 					<h1 class="blog-post_title">XXX</h1>
 					<p class="blog-post_text">
-						XXX
+						We would like to invite you for our interview given on Radio Bogoria!
 					</p>
 					<a href="XXX"
-						class="blog-post_cta">XXX</a>
+						class="blog-post_cta">Facebook Post Link</a>
 				</div>
 			</div>
 			<div class="blog-post">
@@ -146,10 +146,12 @@
 					</div>
 					<h1 class="blog-post_title">XXX</h1>
 					<p class="blog-post_text">
-						XXX
+						Our group just gave an interview for local radio. We would like to invite everyone for the broadcast tomorrow (7.12.2022) at 7:45. Replay of the interview will be aired at 16:45.
+•FM frequency: 94,5
+•Online player link: http://radiobogoria.pl/player
 					</p>
 					<a href="XXX"
-						class="blog-post_cta">XXX</a>
+						class="blog-post_cta">Facebook Post Link</a>
 				</div>
 			</div>
 			<div class="blog-post">
@@ -163,10 +165,10 @@
 					</div>
 					<h1 class="blog-post_title">XXX</h1>
 					<p class="blog-post_text">
-						XXX
+						We got through! After sending our first report (PDR) we kept our fingers crossed for our work and recently got info on passing 1st stage! It's time to move from words to deeds and start building our satellite. We recently started learning how to code an Arduino, which will be CanSat's heart. We are counting on more successes in the future, and we are dreaming of launching our satellite in finals 🚀
 					</p>
 					<a href="XXX"
-						class="blog-post_cta">XXX</a>
+						class="blog-post_cta">Facebook Post Link</a>
 				</div>
 			</div>
 			<div class="blog-post">
@@ -180,10 +182,10 @@
 					</div>
 					<h1 class="blog-post_title">XXX</h1>
 					<p class="blog-post_text">
-						XXX
+						We did it! We just sent our first report of our mission! After a month and a half of working together on PDR (Preliminary Design Review), we can rest a little while waiting for results. The whole team worked on this project alone while overcoming many problems encountered during meetings. We’ve created a strong group of friends, where everybody can trust each other. We keep our fingers crossed for our work, we are counting on moving further in the competition and continuing our adventure with CanSat.
 					</p>
 					<a href="XXX"
-						class="blog-post_cta">XXX</a>
+						class="blog-post_cta">Facebook Post Link</a>
 				</div>
 			</div>
 		</div>
