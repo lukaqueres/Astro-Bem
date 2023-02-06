406e07d82d771a4b6d23311b7cbb64e1d3c032a2
Golombs <xenti12pl@gmail.com>
2023-02-06 18:41:03 +0100

    3
    3

--- web/resources/views/ObserwacjaNieba.blade.php
+++ web/resources/views/ObserwacjaNieba.blade.php
@@ -38,7 +38,7 @@
 						<li class="nav__item"><a href="/" class="nav__link">Home</a></li>
 						<li class="nav__item"><a href="/cansat" class="nav__link">CanSat</a></li>
 						<li class="nav__item"><a href="/articles" class="nav__link">Articles</a></li>
-						<li class="nav__item"><a href="/kosmiczna-szkola" class="nav__link">Kosmiczna szkoła</a></li>
+						<!--<li class="nav__item"><a href="/kosmiczna-szkola" class="nav__link">Kosmiczna szkoła</a></li>-->
 						<li class="nav__item"><a href="/obserwacja-nieba" class="nav__link active">Obserwacja Nieba</a>
 						</li>
 						<li class="nav__item"><a href="/about-us" class="nav__link">About Us</a></li>
