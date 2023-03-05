5c2178e784c9943e8b3a88b9a9ba3362f8314aac
Golombs <xenti12pl@gmail.com>
2023-03-05 15:29:14 +0100

    Update kosmicznaSzkola.blade.php

--- web/resources/views/kosmicznaSzkola.blade.php
+++ web/resources/views/kosmicznaSzkola.blade.php
@@ -109,9 +109,7 @@
 			<div class="blog-post-before">
 				<h1 class="header-text-post">Tak wyglądały początki</h1>
 			</div>
-			<div class="blog-post-before">
-				<h1 class="header-text-post">Test</h1>
-			</div>
+			
 
 			<div class="blog-post">
 
