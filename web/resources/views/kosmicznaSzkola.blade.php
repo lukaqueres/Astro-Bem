5a0ef9dfe6480ec3a8c4233cb26fa159a1d6a972
Golombs <xenti12pl@gmail.com>
2023-03-05 16:18:43 +0100

    Update kosmicznaSzkola.blade.php

--- web/resources/views/kosmicznaSzkola.blade.php
+++ web/resources/views/kosmicznaSzkola.blade.php
@@ -124,6 +124,105 @@
 					</div>
 				</div>
 			</div>
+			<div class="blog-post-before">
+				<h1 class="header-text-post">Dokładne dopasowanie grafiki do ścian</h1>
+			</div>
+			
+
+			<div class="blog-post">
+
+				<div class="blog-grid">	
+					<div class="blog-post_img">
+						<img src="https://raw.githubusercontent.com/lukaqueres/Astro-Bem/abydzialalo/web/public/assets/kosmiczna/1.JPG"  
+							alt="">
+					</div>
+					<div class="blog-post_img">
+						<img src="assets/images/kosmiczna/1.jpg" 
+							alt="">
+					</div>
+				</div>
+			</div>
+			<div class="blog-post-before">
+				<h1 class="header-text-post">Przygotowanie ścian , gładzie oraz gipsowanie</h1>
+			</div>
+			
+
+			<div class="blog-post">
+
+				<div class="blog-grid">	
+					<div class="blog-post_img">
+						<img src="https://raw.githubusercontent.com/lukaqueres/Astro-Bem/abydzialalo/web/public/assets/kosmiczna/1.JPG"  
+							alt="">
+					</div>
+					<div class="blog-post_img">
+						<img src="assets/images/kosmiczna/1.jpg" 
+							alt="">
+					</div>
+				</div>
+			</div>
+
+			<div class="blog-post-before">
+				<h1 class="header-text-post">Ciężka praca fizyczna</h1>
+			</div>
+			
+
+			<div class="blog-post">
+
+				<div class="blog-grid">	
+					<div class="blog-post_img">
+						<img src="https://raw.githubusercontent.com/lukaqueres/Astro-Bem/abydzialalo/web/public/assets/kosmiczna/1.JPG"  
+							alt="">
+					</div>
+					<div class="blog-post_img">
+						<img src="assets/images/kosmiczna/1.jpg" 
+							alt="">
+					</div>
+					<div class="blog-post_img">
+						<img src="assets/images/kosmiczna/1.jpg" 
+							alt="">
+					</div>
+					<div class="blog-post_img">
+						<img src="assets/images/kosmiczna/1.jpg" 
+							alt="">
+					</div>
+				</div>
+			</div>
+			<div class="blog-post-before">
+				<h1 class="header-text-post">Całkowicie nieistotny etap projektowania w Blenderze i renderowania do 2d (realizowany rok)</h1>
+			</div>
+			
+
+			<div class="blog-post">
+
+				<div class="blog-grid">	
+					<div class="blog-post_img">
+						<img src="https://raw.githubusercontent.com/lukaqueres/Astro-Bem/abydzialalo/web/public/assets/kosmiczna/1.JPG"  
+							alt="">
+					</div>
+					<div class="blog-post_img">
+						<img src="assets/images/kosmiczna/1.jpg" 
+							alt="">
+					</div>
+				</div>
+			</div>
+			<div class="blog-post-before">
+				<h1 class="header-text-post">Realizacja bazy 2D</h1>
+			</div>
+			
+
+			<div class="blog-post">
+
+				<div class="blog-grid">	
+					<div class="blog-post_img">
+						<img src="https://raw.githubusercontent.com/lukaqueres/Astro-Bem/abydzialalo/web/public/assets/kosmiczna/1.JPG"  
+							alt="">
+					</div>
+					<div class="blog-post_img">
+						<img src="assets/images/kosmiczna/1.jpg" 
+							alt="">
+					</div>
+				</div>
+			</div>
 		</div>
 
 		@include('templates.footer')
