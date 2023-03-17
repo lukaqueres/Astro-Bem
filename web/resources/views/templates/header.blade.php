e8a3fbf53f73929322fae4edb691ff0184db4226
Golombs <xenti12pl@gmail.com>
2023-03-17 12:14:38 +0100

    Update header.blade.php

--- web/resources/views/templates/header.blade.php
+++ web/resources/views/templates/header.blade.php
@@ -51,8 +51,8 @@
 		<span class="dot" onclick="currentSlide(1)"></span>
 		<span class="dot" onclick="currentSlide(2)"></span>
 		<span class="dot" onclick="currentSlide(3)"></span>
-		<!--<span class="dot" onclick="currentSlide(4)"></span>
-		<span class="dot" onclick="currentSlide(5)"></span>-->
+		<span class="dot" onclick="currentSlide(4)"></span>
+		<!--<span class="dot" onclick="currentSlide(5)"></span>-->
 	</div>
 </div>
 <!-- Ikony -->
